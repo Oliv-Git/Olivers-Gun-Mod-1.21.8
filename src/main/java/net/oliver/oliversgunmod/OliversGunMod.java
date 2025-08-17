38792851fb97b8da11efe2bcb57ef4b247511f8d
OliZoe <jankowskioliver2014@gmail.com>
2025-08-17 19:41:14 +0100

    Initial commit

---
+++ src/main/java/net/oliver/oliversgunmod/OliversGunMod.java
@@ -0,0 +1,17 @@
+package net.oliver.oliversgunmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OliversGunMod implements ModInitializer {
+	public static final String MOD_ID = "olivers-gun-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
